d431446c91b097f4a606c4c2e0e7e37a98c71671
Lisa Ewart <ewartle@gmail.com>
2018-01-07 15:01:34 -0500

    This commit includes the code for all files to complete the logic for the 3 challenges.  I still need to figure out the console.table

--- schema.sql
+++ schema.sql
@@ -24,16 +24,7 @@ INSERT INTO products (product_name, department_name, price, stock_quantity) valu
 INSERT INTO products (product_name, department_name, price, stock_quantity) values ("markers", "school/office supplies", 1, 50);
 
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("diamond necklace", "jewelry", 500, 5);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("pearl earrings", "jewelry", 250, 15);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("wool sweater", "clothing", 40, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("swimsuit", "clothing", 50, 15);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("coffee maker", "homegoods", 30, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("frying pan", "homegoods", 25, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("barbie", "toys", 15, 15);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("twister", "toys" 15, 15);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("notebook", "school/office supplies", 2, 50);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("markers", "school/office supplies", 1, 50);
+
 
 --adding new column to existing table
 ALTER TABLE products,
@@ -51,3 +42,10 @@ over_head_costs INTEGER (11),
 
 PRIMARY KEY (department_id)
 );
+
+INSERT INTO departments (department_name, over_head_costs) values ("jewelry", 5000);
+INSERT INTO departments (department_name, over_head_costs) values ("clothing", 4000);
+INSERT INTO departments (department_name, over_head_costs) values ("homegoods", 9000);
+INSERT INTO departments (department_name, over_head_costs) values ("toys", 2000);
+INSERT INTO departments (department_name, over_head_costs) values ("school/office supplies", 3000);
+
